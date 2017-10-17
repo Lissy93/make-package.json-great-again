0d7f6ce61c1d62c78ed48284ee7b47984df0b704
Alicia Sykes <sykes.alicia@gmail.com>
2017-10-17 14:08:39 +0100

    Initial Commit

---
+++ make-package.json-great-again/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  make-package.json-great-again
+//
+//  Created by Sykes, Alicia on 17/10/2017.
+//  Copyright © 2017 AS93. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ make-package.json-great-again/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  make-package.json-great-again
+//
+//  Created by Sykes, Alicia on 17/10/2017.
+//  Copyright © 2017 AS93. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
